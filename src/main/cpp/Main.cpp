fab0154bd05e024c6b5c52e9d2d487171f810170
Mr. Daehn <jdaehn@missouristate.edu>
2019-03-19 11:19:41 -0500

    LAB06 - Provide demonstration of STL stack.

--- src/main/cpp/Main.cpp
+++ src/main/cpp/Main.cpp
@@ -2,15 +2,21 @@
  * CSC232 Data Structures
  * Missouri State University
  * 
- * @file   Main.cpp
- * @author Jim Daehn
- * @brief  Entry point for hw03 target.
+ * @file    Main.cpp
+ * @authors Jim Daehn <jdaehn@missouristate.edu>
+ *          TODO: Erase this comment and put your name here
+ * @brief   Entry point for hw03 target.
  */
 
 #include <cstdlib>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, main!" << std::endl;
+    // TODO: Use the STL stack to reverse the following string:
+    std::string course{"CSC232 - Data Structures"};
+
+    // TODO: Use the STL stack to check if the following string has balanced braces; print true or false
+    std::string braces{"{abc{de}{fg}hij}kl"};
+
     return EXIT_SUCCESS;
 }
